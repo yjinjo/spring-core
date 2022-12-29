22baaadb6cca33966e4b99f73997e90356219222
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-29 22:35:28 +0900

    #1. 회원 도메인 실행과 테스트

---
+++ src/main/java/hello/springcore/MemberApp.java
@@ -0,0 +1,19 @@
+package hello.springcore;
+
+import hello.springcore.member.Grade;
+import hello.springcore.member.Member;
+import hello.springcore.member.MemberService;
+import hello.springcore.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
